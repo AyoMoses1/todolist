5dbed873d5344ea16ea33a75702a9f36fde7a1ba
Ayo Moses <ayomoses@afrimash.com>
2023-01-26 16:39:06 +0100

    Resolved issues from feedback

--- src/index.js
+++ src/index.js
@@ -30,14 +30,18 @@ const array = [
   },
 ];
 
-array.forEach((arr) => {
-  const task = document.createElement('li');
-  task.innerHTML = `
-                   <div>
-                    <input type="checkbox" id="check" name="task_check">
-                    <label for="task_check">${arr.description}</label>
-                   </div>
-                    <i class="fa-solid fa-ellipsis-vertical"></i>  
-                  `;
-  listPlaceholder.appendChild(task);
-});
+const displayTasks = () => {
+  array.forEach((arr) => {
+    const task = document.createElement('li');
+    task.innerHTML = `
+                     <div>
+                      <input type="checkbox" id="check" name="task_check">
+                      <label for="task_check">${arr.description}</label>
+                     </div>
+                      <i class="fa-solid fa-ellipsis-vertical"></i>  
+                    `;
+    listPlaceholder.appendChild(task);
+  });
+};
+
+displayTasks();
